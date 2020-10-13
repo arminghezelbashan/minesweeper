4fa3952543f0142039b721e92a13279e5c550f81
Armin Ghezelbashan <armin.ghezelbashan@ucalgary.ca>
2020-10-13 17:33:28 -0600

    Updates

---
+++ precache-manifest.b2f09923e37337f3755d407d0c908525.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd30f84e56a59cf910ddadd73f16957",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "7edf115d44cdde3297dc",
+    "url": "/minesweeper/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2788662d66cda2463171",
+    "url": "/minesweeper/static/css/main.a6028c8f.chunk.css"
+  },
+  {
+    "revision": "7edf115d44cdde3297dc",
+    "url": "/minesweeper/static/js/2.a74b4691.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/minesweeper/static/js/2.a74b4691.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2788662d66cda2463171",
+    "url": "/minesweeper/static/js/main.6f1b08f2.chunk.js"
+  },
+  {
+    "revision": "03fdde6e651e9bdac82d",
+    "url": "/minesweeper/static/js/runtime-main.9c76a611.js"
+  }
+]);
